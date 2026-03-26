a861a71c35096237bb893c1e2daaa4021b9b06c6
Nikil Paul S <snikilpaul@gmail.com>
2026-03-26 12:31:20 +0530

    Removing exception as parent gets exception not child

--- custom_input.py
+++ custom_input.py
@@ -2,18 +2,12 @@ def main():
     print("Please send a message to the Redis stream, 'exit' or 'q' ends the program")
 
     while True:
-        try:
-            user_in = input()
-            if "exit" in user_in:
-                print("Ending....")
-                break
-            else:
-                print(user_in)
-
-        except KeyboardInterrupt:
-            print()
-            print("The program was interputed..")
+        user_in = input()
+        if "exit" in user_in:
+            print("Ending....")
             break
+        else:
+            print(user_in)
 
 
 if __name__ == "__main__":
